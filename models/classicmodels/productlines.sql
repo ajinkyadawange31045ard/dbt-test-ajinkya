324477e72300ca27d9a2b443187a38e0166bdc4f
Ajinkya Dawange <ajinkya.dawange@bizacuity.com>
2024-12-09 07:43:43 +0000

    sources added

--- models/classicmodels/productlines.sql
+++ models/classicmodels/productlines.sql
@@ -2,56 +2,39 @@
     materialized='incremental',
     unique_key='productline'
 ) }}
-
-WITH source_data AS (
+WITH batch_metadata AS (
     SELECT
-        productline,
-        create_timestamp,
-        update_timestamp,
-        1001 AS etl_batch_no,
-        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date
-    FROM
-        devstage.productlines
-),
-
-existing_data AS (
-    SELECT
-        productline,
-        src_create_timestamp,
-        src_update_timestamp,
         etl_batch_no,
-        etl_batch_date,
-        dw_update_timestamp,
-        dw_product_line_id
-    FROM
-        devdw.productlines  -- Refers to the current state of the table created by dbt
+        etl_batch_date
+    FROM etl_metadata.batch_control
+    ORDER BY etl_batch_date DESC -- Adjust the order criteria as per your requirement
+    LIMIT 1 -- Fetch only the latest batch
 ),
-
-ranked_data AS (
+processed_data AS (
     SELECT
-        source_data.productline,
-        ROW_NUMBER() OVER (ORDER BY source_data.productline) + COALESCE(MAX(existing_data.dw_product_line_id) OVER (), 0) AS dw_product_line_id,
-        CASE
-            WHEN source_data.productline IS NOT NULL AND existing_data.productline IS NULL THEN source_data.create_timestamp
-            ELSE existing_data.src_create_timestamp
-        END AS src_create_timestamp,
-        COALESCE(source_data.update_timestamp, existing_data.src_update_timestamp) AS src_update_timestamp,
-        1001 AS etl_batch_no,
-        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date,
-        CASE
-            WHEN source_data.productline IS NOT NULL THEN CURRENT_TIMESTAMP
-            ELSE existing_data.dw_update_timestamp
-        END AS dw_update_timestamp,
-        CURRENT_TIMESTAMP AS dw_create_timestamp
-    FROM
-        source_data
-    LEFT JOIN existing_data ON source_data.productline = existing_data.productline
+        st.productline,
+        '' AS textdescription,
+        COALESCE(dw.src_create_timestamp, st.create_timestamp) AS src_create_timestamp,
+        COALESCE(st.update_timestamp, dw.src_update_timestamp) AS src_update_timestamp,
+        batch.etl_batch_no AS etl_batch_no,
+        batch.etl_batch_date AS etl_batch_date,
+        COALESCE(dw.dw_update_timestamp, CURRENT_TIMESTAMP) AS dw_update_timestamp,
+        CURRENT_TIMESTAMP AS dw_create_timestamp,
+        ROW_NUMBER() OVER (ORDER BY st.productline) 
+            + COALESCE(MAX(dw.dw_product_line_id) OVER (), 0) AS dw_product_line_id,
+           dw.productline AS dw_productline 
+    FROM 
+        {{source("devstage","productlines")}} AS st
+    LEFT JOIN {{this}} AS dw
+        ON st.productline = dw.productline
+    CROSS JOIN batch_metadata batch
 )
-
-SELECT *
-FROM ranked_data
+SELECT dw_product_line_id,productline,textdescription,src_create_timestamp,src_update_timestamp,dw_create_timestamp,dw_update_timestamp,
+etl_batch_no,etl_batch_date
+FROM processed_data
 
 {% if is_incremental() %}
-WHERE
-    ranked_data.productline IS NOT NULL  -- Only process new or updated rows
+WHERE 
+    processed_data.src_update_timestamp > processed_data.dw_update_timestamp
+    OR processed_data.dw_productline IS NULL  -- Handle new and updated rows only
 {% endif %}
