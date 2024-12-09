48e2dff354a4262611940c884fc91409b8255787
Ajinkya Dawange <ajinkya.dawange@bizacuity.com>
2024-12-09 07:39:34 +0000

    asdf

--- models/classicmodels/productlines.sql
+++ models/classicmodels/productlines.sql
@@ -3,23 +3,15 @@
     unique_key='productline'
 ) }}
 
-WITH etl_metadata AS (
+WITH source_data AS (
     SELECT
-        etl_batch_no,
-        etl_batch_date
+        productline,
+        create_timestamp,
+        update_timestamp,
+        1001 AS etl_batch_no,
+        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date
     FROM
-        etl_metadata.batch_control  -- Replace with your actual table that holds batch info
-),
-
-source_data AS (
-    SELECT
-        a.productline,
-        a.create_timestamp,
-        a.update_timestamp,
-        b.etl_batch_no,
-        b.etl_batch_date
-    FROM
-        devstage.productlines a cross join etl_metadata b
+        devstage.productlines
 ),
 
 existing_data AS (
@@ -32,28 +24,28 @@ existing_data AS (
         dw_update_timestamp,
         dw_product_line_id
     FROM
-        {{ this }}  -- Refers to the current state of the table created by dbt
+        devdw.productlines  -- Refers to the current state of the table created by dbt
 ),
 
 ranked_data AS (
     SELECT
-        sd.productline,
-        ROW_NUMBER() OVER (ORDER BY sd.productline) + COALESCE(MAX(ed.dw_product_line_id) OVER (), 0) AS dw_product_line_id,
+        source_data.productline,
+        ROW_NUMBER() OVER (ORDER BY source_data.productline) + COALESCE(MAX(existing_data.dw_product_line_id) OVER (), 0) AS dw_product_line_id,
         CASE
-            WHEN sd.productline IS NOT NULL AND ed.productline IS NULL THEN sd.create_timestamp
-            ELSE ed.src_create_timestamp
+            WHEN source_data.productline IS NOT NULL AND existing_data.productline IS NULL THEN source_data.create_timestamp
+            ELSE existing_data.src_create_timestamp
         END AS src_create_timestamp,
-        COALESCE(sd.update_timestamp, ed.src_update_timestamp) AS src_update_timestamp,
-        sd.etl_batch_no,
-        sd.etl_batch_date,
+        COALESCE(source_data.update_timestamp, existing_data.src_update_timestamp) AS src_update_timestamp,
+        1001 AS etl_batch_no,
+        TO_DATE('2001-01-01', 'YYYY-MM-DD') AS etl_batch_date,
         CASE
-            WHEN sd.productline IS NOT NULL THEN CURRENT_TIMESTAMP
-            ELSE ed.dw_update_timestamp
+            WHEN source_data.productline IS NOT NULL THEN CURRENT_TIMESTAMP
+            ELSE existing_data.dw_update_timestamp
         END AS dw_update_timestamp,
         CURRENT_TIMESTAMP AS dw_create_timestamp
     FROM
-        source_data sd
-    LEFT JOIN existing_data ed ON sd.productline = ed.productline
+        source_data
+    LEFT JOIN existing_data ON source_data.productline = existing_data.productline
 )
 
 SELECT *
